67394826e470202ef60a580103f6fe38fcbdb72d
Masaki-G <masasaki3107@gmail.com>
2023-10-26 21:16:24 +0900

    useRecoilを使用

--- client/src/Pages/CreateRoom.jsx
+++ client/src/Pages/CreateRoom.jsx
@@ -1,40 +1,67 @@
-import React, { useState }from 'react';
-import { Flex, Button, Heading, Text, Center, Input, Box} from '@chakra-ui/react';
-import Gbox from '../Components/GlassBox'
+import React, { useState } from 'react';
+import {
+  Flex,
+  Button,
+  Heading,
+  Text,
+  Center,
+  Input,
+  Box,
+} from '@chakra-ui/react';
+import Gbox from '../Components/GlassBox';
 import { useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
+import { useRecoilState } from 'recoil';
+import { userDataState } from '../state/userDataState';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3001');
 
 function CreateRoom() {
-    const [userName, setUserName] = useState('');
-    const navigate = useNavigate();
+  const [userData, setUserData] = useRecoilState(userDataState);
+  const navigate = useNavigate();
 
-    const createRoom = () => {
-        // ランダムなルームIDを生成
-        const roomId = randomRoomIdCreate();
-        // ルーム作成API呼び出し
-        socket.emit('createRoom', roomId, userName);
-        // メイン画面に移動
-        navigate('/Main');
-    };
+  const createRoom = () => {
+    // ランダムなルームIDを生成
+    const roomId = randomRoomIdCreate();
+    // ルーム作成API呼び出し
+    socket.emit('createRoom', roomId, userData);
 
-    const randomRoomIdCreate = () => {
-        return new Date().getTime().toString(16)  + Math.floor(Math.random()).toString(16)
-    }
+    // メイン画面に移動
+    navigate('/Main');
+  };
+
+  const randomRoomIdCreate = () => {
+    return (
+      new Date().getTime().toString(16) + Math.floor(Math.random()).toString(16)
+    );
+  };
 
   return (
     <Flex
-        height="100vh" 
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
     >
-        <Gbox>
-            <Text as="b">名前を入力して、ルームを作成してください</Text>
-            <Input onChange={(e) => setUserName(e.target.value)} placeholder='ニックネーム' w="22rem" mt="5" mb="35" />
-            <Button onClick={createRoom} colorScheme="yellow" w="22rem" size="lg" border="none">ルームを作成</Button>
-        </Gbox>
+      <Gbox>
+        <Text as="b">名前を入力して、ルームを作成してください</Text>
+        <Input
+          onChange={e => setUserData(e.target.value)}
+          placeholder="ニックネーム"
+          w="22rem"
+          mt="5"
+          mb="35"
+        />
+        <Button
+          onClick={createRoom}
+          colorScheme="yellow"
+          w="22rem"
+          size="lg"
+          border="none"
+        >
+          ルームを作成
+        </Button>
+      </Gbox>
     </Flex>
   );
 }
