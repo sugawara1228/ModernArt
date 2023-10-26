b510998bd93a7e5f17301283516969c584734026
Masaki-G <masasaki3107@gmail.com>
2023-10-26 21:16:09 +0900

    useRecoilを使えるように

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,18 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import theme from "./Theme";
-
+import theme from './Theme';
+import { RecoilRoot } from 'recoil';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <ChakraProvider theme={theme}>
-    <ColorModeScript />
-    <App />
+    <RecoilRoot>
+      <ColorModeScript />
+      <App />
+    </RecoilRoot>
   </ChakraProvider>
 );
 
